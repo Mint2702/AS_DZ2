b55e3f91bbc6dd9e0a29b27ccfee6813b42fcbc4
Danil <danil.mirtalibov.27@mail.ru>
2020-10-09 20:34:17 +0300

    Modified objects for articles. Output uses ojects fully now.

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -6,59 +6,43 @@ button_show_All.addEventListener('click', showAll)
 let flag = false;
 
 function showAll(){
-    let inserted_data1 = `<div id="part1">
-    <div class="text1">
-      See next city: <a href="#part2">London</a>
-      <div class="Logo1">
-        Moscow
-      </div>
-      <div class="Main1">
-        I lived in Moscow for the longest period of my live, around 10 years.<br>
-        Love this city beacuse of it's beauty and importance to me. A lot of really important events happend to me in this city.
-      </div>
-      <div class="image">
-        <div class="ImagesColumn">
-          <img src="static/images/MoscowCity1.jpg" style="width:80%;height:45%;" >
-          <img src="static/images/Luzhniki.jpg" style="width:80%;height:45%;">
-        </div>
-        <div class="ImagesSpace">
-          <img src="static/images/moscow1.webp" style="width:60%;height:30%;">
-          <img src="static/images/nikolskaya.jpg" style="width:60%;height:30%;">
-          <img src="static/images/carskaya-ploshcad-moskva-jk-842327202-6.jpg" style="width:60%;height:30%;">
-        </div>
-      </div>
-    </div>
-    </div>
-    `
-    let inserted_data2 = `<div id="part2">
-    <div class="text2">
-    See previous city: <a href="#part1">Moscow</a>
-    <div class="Logo2">
-      London
-    </div>
-    <div class="Main2">
-      London was home for me for 4 years.<br>
-      It's very importa for me beacuse it influenced me a lot. Here are some pictures of the area where I lived(Camden Town).
-    </div>
-    <div class="image">
-      <div class="ImagesColumn">
-        <img src="static/images/london1.jpg" style="width:90%;height:45%;" >
-        <img src="static/images/london2.jpg" style="width:90%;height:45%;">
-      </div>
-      <div class="ImagesSpace">
-        <img src="static/images/Camden_Town.jpg" style="width:60%;height:30%;">
-        <img src="static/images/camden2.jpg" style="width:60%;height:30%;">
-        <img src="static/images/camden3.png" style="width:60%;height:30%;">
-      </div>
-    </div>
-  </div>
-    </div>
-    `
+    let len=articles.length
     if (flag == false){
-        content.insertAdjacentHTML('afterend', inserted_data2);
-        content.insertAdjacentHTML('afterend', inserted_data1);
         flag = true;
+        for (let i=len-1;i>=0;i--){
+            let a=i
+            if (i==0){
+                a=len-1
+            } 
+            else{
+                a=i-1;
+            } 
+            let inserted_data = `
+            <div id=${articles[i].part}>
+                <div class="${articles[i].textId}">
+                    See next city: <a href="#${articles[a].part}">${articles[a].city}</a>
+                    <div class=${articles[i].logo}>
+                        ${articles[i].city}
+                    </div>
+                    <div class=${articles[i].main}>
+                        ${articles[i].text}
+                    </div>
+                    <div class="image">
+                        <div class="ImagesColumn">
+                            <img src=${articles[i].imagesC[0]} style="width:${articles[i].width}%;height:45%;" >
+                            <img src=${articles[i].imagesC[1]} style="width:${articles[i].width}%;height:45%;">
+                        </div>
+                        <div class="ImagesSpace">
+                            <img src=${articles[i].imagesS[0]} style="width:60%;height:30%;">
+                            <img src=${articles[i].imagesS[1]} style="width:60%;height:30%;">
+                            <img src=${articles[i].imagesS[2]} style="width:60%;height:30%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        content.insertAdjacentHTML('afterend', inserted_data);
+        }
     } 
     else alert("All articles are shown already")
-    
 }
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,5 +1,25 @@
 let moscow = {
+    part : 'part1',
+    textId : 'text1',
     city : 'Moscow',
+    logo : "Logo1",
+    main : "Main1",
     text : "I lived in Moscow for the longest period of my live, around 10 years.<br>Love this city beacuse of it's beauty and importance to me. A lot of really important events happend to me in this city.",
-    
+    imagesC : ["static/images/MoscowCity1.jpg","static/images/Luzhniki.jpg"],
+    width : '80',
+    imagesS : ["static/images/moscow1.webp","static/images/nikolskaya.jpg","static/images/carskaya-ploshcad-moskva-jk-842327202-6.jpg"]
 }
+
+let london = {
+    part : 'part2',
+    textId : 'text2',
+    logo : "Logo2",
+    city : 'London',
+    main : "Main2",
+    text : "London was home for me for 4 years.<br>It's very importa for me beacuse it influenced me a lot. Here are some pictures of the area where I lived(Camden Town).",
+    imagesC : ["static/images/london1.jpg","static/images/london2.jpg"],
+    width : '90',
+    imagesS : ["static/images/Camden_Town.jpg","static/images/camden2.jpg","static/images/camden3.png"]
+}
+
+let articles = [moscow,london]
